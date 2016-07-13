e264dabd8fabcd96e178af6a6bbfc4cd1c2c7f68
Khanh Duc Tran <ktran035@uw.edu>
2016-07-13 18:30:45 +0700

    retrive some cookies for notification

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
 window.onload = function () {
-	setInterval(refrestData, 5000);
+	setInterval(refreshData, 5000);
 }
-function refrestData() {
-
+function refreshData() {
+	InOrNot = false;
+	chrome.cookies.getAll({"domain":".facebook.com"}, function(cookies) {
+		// run through all available cookies that got from facebook
+		for (var i = 0; i < cookies.length; i++) {
+			// determine if the current cookies is one that tell us either user is logged in or not
+			if (cookies[i].name == "c_user" ) {
+				InOrNot = true;
+				console.log(localStorage.getItem("c_user"));
+			}
+		}
+	})
 }
